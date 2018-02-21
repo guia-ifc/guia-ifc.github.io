cc7adf602b62a9acac1cd8deb35966bfbb8ec644
CAAT <caat.ifc@gmail.com>
2018-02-20 22:36:23 -0300

    Conserto do animate bugado :(

--- js/agency.js
+++ js/agency.js
@@ -1,16 +1,22 @@
 (function($) {
   "use strict"; // Start of use strict
 
+  var canClick = true;
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 54)
-        }, 1000, "easeInOutExpo");
-        return false;
+    if (canClick) {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          canClick = false;
+          $('html, body').animate({
+            scrollTop: (target.offset().top - 54)
+          }, 1000, "easeInOutExpo", function() {
+            canClick = true;
+          });
+          return false;
+        }
       }
     }
   });
