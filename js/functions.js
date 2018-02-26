19cdb344366277d758471086b69505fbb8184586
CAAT <caat.ifc@gmail.com>
2018-02-25 23:31:28 -0300

    Scroll em dúvidas só no Mobile

--- js/functions.js
+++ js/functions.js
@@ -55,8 +55,33 @@
 					}, 400, 'easeInOutExpo', function () {
 						$conteudo.html( data );
 
+						var isMobile = {
+						    Android: function() {
+						        return navigator.userAgent.match(/Android/i);
+						    },
+						    BlackBerry: function() {
+						        return navigator.userAgent.match(/BlackBerry/i);
+						    },
+						    iOS: function() {
+						        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+						    },
+						    Opera: function() {
+						        return navigator.userAgent.match(/Opera Mini/i);
+						    },
+						    Windows: function() {
+						        return navigator.userAgent.match(/IEMobile/i);
+						    },
+						    any: function() {
+						        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+						    }
+						};
+
+						console.log(isMobile.any())
+
 						if ($self.closest('#sidebar2').length) {
-							$("html, body").animate({ scrollTop: $('#sidebar2').offset().top + 350 }, 300);
+							if( isMobile.any() ) {
+								$("html, body").animate({ scrollTop: $('#sidebar2').offset().top + 350 }, 300);							 
+							}
 						}
 
 					});			
